97e38e4ca162a1109ec3f8563be41a2ab64148e8
winterofchoi <87969359+winterofchoi@users.noreply.github.com>
2022-07-17 00:14:10 +0900

    Update randomeNumber.js

--- randomeNumber.js
+++ randomeNumber.js
@@ -1,2 +1,2 @@
 let random = Math.floor(Math.random() * 4) + 1;
-document.querySelector('img_box').classList.add(bg0 + randomeNumber);
+document.background_image('gif_box').classList.add(bg0 + randomeNumber);
