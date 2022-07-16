853e77c0fc5d7addfbb584d43e00458368dcefa5
winterofchoi <87969359+winterofchoi@users.noreply.github.com>
2022-07-17 00:49:36 +0900

    Add files via upload

--- randomeNumber.js
+++ randomeNumber.js
@@ -1,4 +1,4 @@
-let random = Math.floor(Math.random() * 4) + 1;
-document.background_image('gif_box').classList.add(bg0 + randomeNumber);
+let randomNumber = Math.floor(Math.random() * 4) + 1;
+document.querySelection('.gif_box').classList.add('bg0' + randomeNumber);
 
 
